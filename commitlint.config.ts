4b1f3f468512e8309a8ca481b5c8933a72fcd56f
Emon <biplobemon.dev@gmail.com>
2026-05-03 15:34:37 +0600

    ci: add development to main auto deploy workflow

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,24 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "chore", "refactor", "style", "docs", "test"]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "refactor",
+        "style",
+        "docs",
+        "test",
+        "ci",
+        "perf",
+        "build",
+        "revert",
+        "workflow",
+      ],
+    ],
   },
 };
 
